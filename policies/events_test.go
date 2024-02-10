62af609951e3bc61929630520aa4850a1bbdab1e
Niall Young <5465765+niallyoung@users.noreply.github.com>
2024-02-10 20:06:36 +1100

    events_test.go PreventTimestampsInThePast

--- policies/events_test.go
+++ policies/events_test.go
@@ -33,7 +33,7 @@ func TestPreventTimestampsInTheFuture(t *testing.T) {
 		assert.Equal(t, "", result)
 	})
 
-	t.Run("future event above threshold returns false", func(t *testing.T) {
+	t.Run("future event above threshold returns true", func(t *testing.T) {
 		const thresholdSeconds int64 = 120
 		thresholdTimestamp := nostr.Timestamp(thresholdSeconds)
 		futureTimestamp := time.Now().Add(240 * time.Second).Unix()
@@ -56,6 +56,59 @@ func TestPreventTimestampsInTheFuture(t *testing.T) {
 			futureEvent := &nostr.Event{CreatedAt: nostr.Timestamp(futureTimestamp)}
 
 			fn := policies.PreventTimestampsInTheFuture(thresholdTimestamp)
+			ok, result := fn(context.Background(), futureEvent)
+
+			assert.Equal(t, false, ok)
+			assert.Equal(t, "", result)
+		}
+	})
+}
+
+func TestPreventTimestampsInThePast(t *testing.T) {
+	t.Run("Now() returns false with no msg", func(t *testing.T) {
+		fn := policies.PreventTimestampsInThePast(nostr.Now())
+
+		ok, result := fn(context.Background(), &nostr.Event{})
+		assert.Equal(t, false, ok)
+		assert.Equal(t, "", result)
+	})
+
+	t.Run("past event below threshold returns false", func(t *testing.T) {
+		const thresholdSeconds int64 = 120
+		thresholdTimestamp := nostr.Timestamp(thresholdSeconds)
+		pastTimestamp := time.Now().Add(-60 * time.Second).Unix()
+		pastEvent := &nostr.Event{CreatedAt: nostr.Timestamp(pastTimestamp)}
+
+		fn := policies.PreventTimestampsInThePast(thresholdTimestamp)
+		ok, result := fn(context.Background(), pastEvent)
+
+		assert.Equal(t, false, ok)
+		assert.Equal(t, "", result)
+	})
+
+	t.Run("past event above threshold returns true", func(t *testing.T) {
+		const thresholdSeconds int64 = 120
+		thresholdTimestamp := nostr.Timestamp(thresholdSeconds)
+		pastTimestamp := time.Now().Add(-240 * time.Second).Unix()
+		pastEvent := &nostr.Event{CreatedAt: nostr.Timestamp(pastTimestamp)}
+
+		fn := policies.PreventTimestampsInThePast(thresholdTimestamp)
+		ok, result := fn(context.Background(), pastEvent)
+
+		assert.Equal(t, true, ok)
+		assert.Equal(t, "event too old", result)
+	})
+
+	t.Run("future events always return false", func(t *testing.T) {
+		const thresholdSeconds int64 = 120
+		negativeInts := []int64{0, 5, 20, 100, 1000, 9999}
+
+		for _, s := range negativeInts {
+			thresholdTimestamp := nostr.Timestamp(thresholdSeconds)
+			futureTimestamp := time.Now().Add(time.Duration(s) * time.Second).Unix()
+			futureEvent := &nostr.Event{CreatedAt: nostr.Timestamp(futureTimestamp)}
+
+			fn := policies.PreventTimestampsInThePast(thresholdTimestamp)
 			ok, result := fn(context.Background(), futureEvent)
 
 			assert.Equal(t, false, ok)
