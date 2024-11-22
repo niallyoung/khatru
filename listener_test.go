29e905d864a3db0b3806723de180045b977b1180
Niall Young <5465765+niallyoung@users.noreply.github.com>
2024-11-23 07:34:06 +1100

    listener_test.go, 51.3%

--- listener_test.go
+++ listener_test.go
@@ -9,11 +9,11 @@ import (
 )
 
 func TestGetListeningFilters(t *testing.T) {
-	t.Run("TBC", func(t *testing.T) {
+	t.Run("get listening filters with no listeners returns empty", func(t *testing.T) {
 
 		filters := GetListeningFilters()
 		emptyFilters := make(nostr.Filters, 0, listeners.Size()*2)
 		assert.Equal(t, emptyFilters, filters)
-		//assert.IsType(t, []nostr.Filter, filters)
+		assert.Len(t, filters, 0)
 	})
 }
